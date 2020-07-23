8dd49f9432c3c3683c37415af6832a67f46a570a
yypz <1433752013@qq.com>
2020-07-23 15:02:25 +0800

    TEST

--- eurekaDiscovery/eureka-server/src/main/java/com/ye/erkserver/controller/HelloServerController.java
+++ eurekaDiscovery/eureka-server/src/main/java/com/ye/erkserver/controller/HelloServerController.java
@@ -1,12 +1,11 @@
 package com.ye.erkserver.controller;
 
-import java.util.List;
-import java.util.Random;
-
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloServerController {
@@ -15,13 +14,31 @@ public class HelloServerController {
 	DiscoveryClient client;
 
 	@RequestMapping("/getHello")
-	public String hello() {
+	public UserEntity hello(@RequestBody(required=false) UserEntity user) {
 		/*List<String> ff=client.getServices();
 		List<ServiceInstance> instances = client.getInstances("HelloServer");
 		ServiceInstance selectedInstance = instances.get(new Random().nextInt(instances.size()));
 		return "Hello World: " + selectedInstance.getServiceId() + ":" + selectedInstance.getHost() + ":"
-				+ selectedInstance.getPort();*/
-		return "ffffff";
+				+ selectedInstance.getPort();
+		try {
+			long l=System.currentTimeMillis();
+			System.out.println(l+" start ");
+			Thread.sleep(5000);
+			System.out.println(l+" over ");
+
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		String ff=null;
+		if (user !=null) {
+			System.err.println(user.getName() + user.getAge());
+		}else {
+			user=new UserEntity();
+			user.setName("hhhh");
+		}
+		
+		return user;
 	}
 
 }
